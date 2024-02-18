64fb420c90934b42d879027be49799eba3f585c3
Madeleine Garbin <madeleine.garbin@kellogg.northwestern.edu>
2024-02-18 00:41:31 +0000

    avance

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -13,6 +13,21 @@ def show
 
 end
 
+# render view with new place form
+def new
+end
+
+#create new place
+def create
+  # start with a new Place
+  @place = Place.new
+  # assign user-entered form data to Place's columns
+  @place["name"] = params["name"]
+  # save row
+  @place.save
+  # redirect user
+  redirect_to "/places"
+end
 
 
   end
